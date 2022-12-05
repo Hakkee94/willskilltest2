3d7d518de88ee527d6c9aa0406087dd9737c799d
d.agasyan <d.agasyan@getproduction.ru>
2022-12-05 16:58:24 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
 import './App.css';
 import Header from "./components/Header";
 import YellowSection from "./components/YellowSection";
 import FinalSection from "./components/FinalSection";
 
-function App() {
+function App(){
     return (
         <div className="App">
             <Header/>
